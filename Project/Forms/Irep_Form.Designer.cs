8259ccdfcfdd38d49c5e863b85ed3f56012109ae
Abdelrahman Torky <24torky@gmail.com>
2022-04-02 02:40:46 +0200

    -Finished

--- Project/Forms/Irep_Form.Designer.cs
+++ Project/Forms/Irep_Form.Designer.cs
@@ -30,18 +30,18 @@ namespace Project.Forms
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.rep1_btn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.rep4_btn = new System.Windows.Forms.Button();
-            this.rep6_btn = new System.Windows.Forms.Button();
-            this.rep3_btn = new System.Windows.Forms.Button();
-            this.rep2_btn = new System.Windows.Forms.Button();
             this.rep5_btn = new System.Windows.Forms.Button();
+            this.rep2_btn = new System.Windows.Forms.Button();
+            this.rep3_btn = new System.Windows.Forms.Button();
+            this.rep6_btn = new System.Windows.Forms.Button();
+            this.rep4_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rep1_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,81 +60,76 @@ namespace Project.Forms
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.rep1_btn);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(143, 40);
+            this.panel1.Location = new System.Drawing.Point(163, 53);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(573, 410);
+            this.panel1.Size = new System.Drawing.Size(655, 547);
             this.panel1.TabIndex = 0;
             // 
-            // label2
+            // rep5_btn
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
-            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.Maroon;
-            this.label2.Location = new System.Drawing.Point(99, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Report 1 :";
+            this.rep5_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep5_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep5_btn.Location = new System.Drawing.Point(306, 356);
+            this.rep5_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep5_btn.Name = "rep5_btn";
+            this.rep5_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep5_btn.TabIndex = 16;
+            this.rep5_btn.Text = "Open";
+            this.rep5_btn.UseVisualStyleBackColor = true;
+            this.rep5_btn.Click += new System.EventHandler(this.rep5_btn_Click);
             // 
-            // rep1_btn
+            // rep2_btn
             // 
-            this.rep1_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep1_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep1_btn.Location = new System.Drawing.Point(268, 45);
-            this.rep1_btn.Name = "rep1_btn";
-            this.rep1_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep1_btn.TabIndex = 6;
-            this.rep1_btn.Text = "Open";
-            this.rep1_btn.UseVisualStyleBackColor = true;
+            this.rep2_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep2_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep2_btn.Location = new System.Drawing.Point(306, 135);
+            this.rep2_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep2_btn.Name = "rep2_btn";
+            this.rep2_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep2_btn.TabIndex = 15;
+            this.rep2_btn.Text = "Open";
+            this.rep2_btn.UseVisualStyleBackColor = true;
+            this.rep2_btn.Click += new System.EventHandler(this.rep2_btn_Click);
             // 
-            // label1
+            // rep3_btn
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
-            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.Maroon;
-            this.label1.Location = new System.Drawing.Point(99, 332);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 24);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Report 6 :";
+            this.rep3_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep3_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep3_btn.Location = new System.Drawing.Point(306, 207);
+            this.rep3_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep3_btn.Name = "rep3_btn";
+            this.rep3_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep3_btn.TabIndex = 14;
+            this.rep3_btn.Text = "Open";
+            this.rep3_btn.UseVisualStyleBackColor = true;
+            this.rep3_btn.Click += new System.EventHandler(this.rep3_btn_Click);
             // 
-            // label3
+            // rep6_btn
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
-            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.ForeColor = System.Drawing.Color.Maroon;
-            this.label3.Location = new System.Drawing.Point(99, 278);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 24);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Report 5 :";
+            this.rep6_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep6_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep6_btn.Location = new System.Drawing.Point(306, 428);
+            this.rep6_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep6_btn.Name = "rep6_btn";
+            this.rep6_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep6_btn.TabIndex = 13;
+            this.rep6_btn.Text = "Open";
+            this.rep6_btn.UseVisualStyleBackColor = true;
+            this.rep6_btn.Click += new System.EventHandler(this.rep6_btn_Click);
             // 
-            // label4
+            // rep4_btn
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
-            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.ForeColor = System.Drawing.Color.Maroon;
-            this.label4.Location = new System.Drawing.Point(99, 166);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 24);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Report 3 :";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
-            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.ForeColor = System.Drawing.Color.Maroon;
-            this.label5.Location = new System.Drawing.Point(99, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 24);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Report 2 :";
+            this.rep4_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep4_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep4_btn.Location = new System.Drawing.Point(306, 280);
+            this.rep4_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep4_btn.Name = "rep4_btn";
+            this.rep4_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep4_btn.TabIndex = 12;
+            this.rep4_btn.Text = "Open";
+            this.rep4_btn.UseVisualStyleBackColor = true;
+            this.rep4_btn.Click += new System.EventHandler(this.rep4_btn_Click);
             // 
             // label6
             // 
@@ -142,74 +137,93 @@ namespace Project.Forms
             this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
             this.label6.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.Color.Maroon;
-            this.label6.Location = new System.Drawing.Point(99, 221);
+            this.label6.Location = new System.Drawing.Point(113, 295);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 24);
+            this.label6.Size = new System.Drawing.Size(127, 29);
             this.label6.TabIndex = 11;
             this.label6.Text = "Report 4 :";
             // 
-            // rep4_btn
+            // label5
             // 
-            this.rep4_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep4_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep4_btn.Location = new System.Drawing.Point(268, 210);
-            this.rep4_btn.Name = "rep4_btn";
-            this.rep4_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep4_btn.TabIndex = 12;
-            this.rep4_btn.Text = "Open";
-            this.rep4_btn.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(113, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Report 2 :";
             // 
-            // rep6_btn
+            // label4
             // 
-            this.rep6_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep6_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep6_btn.Location = new System.Drawing.Point(268, 321);
-            this.rep6_btn.Name = "rep6_btn";
-            this.rep6_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep6_btn.TabIndex = 13;
-            this.rep6_btn.Text = "Open";
-            this.rep6_btn.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(113, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Report 3 :";
             // 
-            // rep3_btn
+            // label3
             // 
-            this.rep3_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep3_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep3_btn.Location = new System.Drawing.Point(268, 155);
-            this.rep3_btn.Name = "rep3_btn";
-            this.rep3_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep3_btn.TabIndex = 14;
-            this.rep3_btn.Text = "Open";
-            this.rep3_btn.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(113, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Report 5 :";
             // 
-            // rep2_btn
+            // label1
             // 
-            this.rep2_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep2_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep2_btn.Location = new System.Drawing.Point(268, 101);
-            this.rep2_btn.Name = "rep2_btn";
-            this.rep2_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep2_btn.TabIndex = 15;
-            this.rep2_btn.Text = "Open";
-            this.rep2_btn.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(113, 443);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Report 6 :";
             // 
-            // rep5_btn
+            // rep1_btn
             // 
-            this.rep5_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.rep5_btn.ForeColor = System.Drawing.Color.Maroon;
-            this.rep5_btn.Location = new System.Drawing.Point(268, 267);
-            this.rep5_btn.Name = "rep5_btn";
-            this.rep5_btn.Size = new System.Drawing.Size(159, 35);
-            this.rep5_btn.TabIndex = 16;
-            this.rep5_btn.Text = "Open";
-            this.rep5_btn.UseVisualStyleBackColor = true;
+            this.rep1_btn.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rep1_btn.ForeColor = System.Drawing.Color.Maroon;
+            this.rep1_btn.Location = new System.Drawing.Point(306, 60);
+            this.rep1_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rep1_btn.Name = "rep1_btn";
+            this.rep1_btn.Size = new System.Drawing.Size(182, 47);
+            this.rep1_btn.TabIndex = 6;
+            this.rep1_btn.Text = "Open";
+            this.rep1_btn.UseVisualStyleBackColor = true;
+            this.rep1_btn.Click += new System.EventHandler(this.rep1_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(175)))), ((int)(((byte)(157)))));
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(113, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Report 1 :";
             // 
             // Irep_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
-            this.ClientSize = new System.Drawing.Size(863, 484);
+            this.ClientSize = new System.Drawing.Size(986, 645);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Irep_Form";
             this.Text = "Irep_Form";
             this.panel1.ResumeLayout(false);
--- Project/Forms/Irep_Form.cs
+++ Project/Forms/Irep_Form.cs
@@ -14,5 +14,53 @@ namespace Project.Forms
         {
             InitializeComponent();
         }
+
+        private void rep1_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/CourseTopics";
+            p.Start();
+        }
+
+        private void rep2_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/ExamDetails";
+            p.Start();
+        }
+
+        private void rep3_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/InstructorCourses";
+            p.Start();
+        }
+
+        private void rep4_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/StudentsAnswer";
+            p.Start();
+        }
+
+        private void rep5_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/StudentsGrade";
+            p.Start();
+        }
+
+        private void rep6_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.FileName = "http://localhost/Reports/report/ExamReports/StudentDept";
+            p.Start();
+        }
     }
 }
